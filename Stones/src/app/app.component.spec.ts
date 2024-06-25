4069c10ab7d0c2eb3968ac199e11095199f396dd
Alexander Grigat <alexander.grigat@sn-invent.de>
2024-06-25 10:09:13 +0200

    Aufgabe 2.1

--- Stones/src/app/app.component.spec.ts
+++ Stones/src/app/app.component.spec.ts
@@ -1,6 +1,7 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { ProductComponent } from './product/product/product.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
@@ -9,7 +10,7 @@ describe('AppComponent', () => {
         RouterModule.forRoot([])
       ],
       declarations: [
-        AppComponent
+        AppComponent, ProductComponent
       ],
     }).compileComponents();
   });
@@ -20,16 +21,4 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'stones'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('stones');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, stones');
-  });
 });
