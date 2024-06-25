514e082f1a3d57861ba72207ea4178e62d59b1fb
Alexander Grigat <alexander.grigat@sn-invent.de>
2024-06-25 11:08:25 +0200

    Aufgabe 2.2

--- Stones/src/app/app.component.ts
+++ Stones/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from './product/product';
 
 @Component({
   selector: 'stn-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'stones';
+  parentProduct = new Product(12, "Granitstein Grabo", 120.50, 12);
+
+  onPriceChanged(price: number) {
+    alert('Neuer Preis: ' + price);
+  }
 }
--- Stones/src/app/product/product/product.component.spec.ts
+++ Stones/src/app/product/product/product.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProductComponent } from './product.component';
+import { Product } from '../product';
+import { By } from '@angular/platform-browser';
 
 describe('ProductComponent', () => {
   let component: ProductComponent;
@@ -14,11 +16,18 @@ describe('ProductComponent', () => {
 
     fixture = TestBed.createComponent(ProductComponent);
     component = fixture.componentInstance;
+    component.product = new Product(1,'Teststein', 2,5)
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  
+  it('should correctly show name in DOM', () => {
+    const liName = fixture.debugElement.query(By.css('#name'));
+
+    expect(liName.nativeElement.textContent).toContain(component.product.name);
   });
   
   it('raisePrice should raise price by 5', () => {
