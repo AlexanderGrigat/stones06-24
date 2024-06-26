e4c44b530703c29d3ed65c8389131e42cdb251fb
Alexander Grigat <alexander.grigat@sn-invent.de>
2024-06-26 11:02:52 +0200

    Aufgabe 6.4

--- Stones/src/app/product/product-form/product-form.component.ts
+++ Stones/src/app/product/product-form/product-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Output, inject } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Product } from '../product';
+import { CustomValidators } from '../../utils/validators/custom-validators';
 
 @Component({
   selector: 'stn-product-form',
@@ -18,14 +19,19 @@ export class ProductFormComponent {
 
   private fb = inject(FormBuilder);
   productForm = this.fb.group({
-    name: [''],
-    price: [0],
-    weight: [0],
+    name: ['', [Validators.required, CustomValidators.alphaNum]],
+    price: [0, [Validators.required, CustomValidators.positiv]],
+    weight: [0, Validators.required],
   });
 
   save() {
     const formValue = this.productForm.value;
-    if (formValue.name && formValue.price && formValue.weight) {
+    if (
+      this.productForm.valid &&
+       formValue.name && 
+       formValue.price &&
+        formValue.weight
+      ) {
       const product = new Product(
         -1,
         formValue.name,
